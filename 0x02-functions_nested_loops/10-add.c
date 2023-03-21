995f4e5bb5a224c8151208e0815974dcfedd874b
souhailas142 <souhailas142@gmail.com>
2023-03-21 12:54:14 -0700

    sum to numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @n1: number 1
+ * @n2: number 2
+ * Return: 0 the result
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
