12f1f14e07f70fdd2d5d8381a73adff9fa17138d
souhailas142 <souhailas142@gmail.com>
2023-03-21 11:34:38 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,29 +5,32 @@
  */
 void times_table(void)
 {
-int i = 0, j, m;
-while (i < 10)
-{
-j = 0;
-while (j < 10)
-{
-m = i*j;
-if (m < 10)
-{
-_putchar(' ');
-}
-else
-{
-_putchar(48 + (m/10));
-}
-_putchar(48 + (m%10));
-j++;
-if (j != 10){
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-i++;
-}
+int i = 0;
+	int j = 1;
+	int num = 0;
+
+	while (i <= 9)
+	{
+		putchar('0');
+		putchar(',');
+		putchar(' ');
+		while (j <= 9)
+		{
+			num = i * j;
+			if ((num / 10) > 0)
+				putchar((num / 10) + '0');
+			else
+				putchar(' ');
+			putchar((num % 10) + '0');
+			if (j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		putchar('\n');
+		i++;
+		j = 1;
+	}
 }
