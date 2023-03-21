f7a0e25564df00f0240f1f1da7189771dd554400
souhailas142 <souhailas142@gmail.com>
2023-03-21 06:36:40 -0700

    the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * main - prints the alphabet, in lowercase
+ * print_alphabet - prints the alphabet, in lowercase
  * Return: 0
  *
  */
 void print_alphabet(void)
 {
 	char c = 'a';
+	
 	while (c <= 'z')
 	{
 		_putchar(c);
