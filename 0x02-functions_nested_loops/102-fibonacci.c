1816e32771d4f4b29bf5cb9d6f97bc8d064cdfd1
souhailas142 <souhailas142@gmail.com>
2023-03-21 16:38:35 -0700

    fibanacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 /**
  * main - the first 50 Fibonacci numbers
- * 
  * Return: always 0
  *
  */
 int main(void)
 {
-        unsigned long f = 1;
-	unsigned long s = 2;
-	int i = 0;
-
-	while (i < 50)
-	{
-		printf("%lu", f);
-		if (i < 49)
-		{
-			s += f;
-			f = s - f;
-			printf(", ");
-		}
-		i++;
-	}
-	printf("\n");
-	return (0);
+unsigned long f = 1;
+unsigned long s = 2;
+int i = 0;
+while (i < 50)
+{
+printf("%lu", f);
+if (i < 49)
+{
+s += f;
+f = s - f;
+printf(", ");
+}
+i++;
+}
+printf("\n");
+return (0);
 }
