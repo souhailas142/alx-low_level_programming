032b4b3555d98a8d17b31d43fd7c2b943f7743d2
souhailas142 <souhailas142@gmail.com>
2023-03-21 15:58:42 -0700

    the sum of all the multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_natural_numbers - prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: the sum
+ */
+void print_natural_numbers(void)
+{
+	int sum = 0;
+	int i = 0;
+	while (i <= 1024)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
