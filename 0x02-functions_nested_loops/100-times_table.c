dcd1ffab042039736017bf5577ec2bb077a8f8ac
souhailas142 <souhailas142@gmail.com>
2023-03-21 14:11:47 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,53 +6,52 @@
  */
 void print_times_table(int n)
 {
-	int i = 0;
-	int j = 1;
-	int num = 0;
-	int tmp = 0;
-
-	if (n > 15 || n < 0)
-		return;
-	while (i <= n)
-	{
-		putchar('0');
-		if (n != 0)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		while (j <= n)
-	{
-		num = i * j;
-		if ((num / 10) > 0)
-		{
-			tmp = (num / 10);
-			if (tmp > 9)
-			{
-				putchar((tmp / 10) + '0');
-				putchar((tmp % 10) + '0');
-			}
-			else
-			{
-				putchar(' ');
-				putchar((num / 10) + '0');
-			}
-		}
-		else
-		{
-			putchar(' ');
-			putchar(' ');
-		}
-		putchar((num % 10) + '0');
-		if (j != n)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		j++;
-	}
-		putchar('\n');
-		i++;
-		j = 1;
-	}
+int i = 0;
+int j = 1;
+int num = 0;
+int tmp = 0;
+if (n > 15 || n < 0)
+return;
+while (i <= n)
+{
+_putchar('0');
+if (n != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+while (j <= n)
+{
+num = i * j;
+if ((num / 10) > 0)
+{
+tmp = (num / 10);
+if (tmp > 9)
+{
+_putchar((tmp / 10) + '0');
+_putchar((tmp % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((num / 10) + '0');
+}
+}
+else
+{
+_putchar(' ');
+_putchar(' ');
+}
+_putchar((num % 10) + '0');
+if (j != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+j++;
+}
+_putchar('\n');
+i++;
+j = 1;
+}
 }
