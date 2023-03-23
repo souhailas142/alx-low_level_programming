cbf030c5e462de151ee4fd552967e079fde04bb0
souhailas142 <souhailas142@gmail.com>
2023-03-23 03:00:40 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int i = 48;
+while (i <= 57)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
