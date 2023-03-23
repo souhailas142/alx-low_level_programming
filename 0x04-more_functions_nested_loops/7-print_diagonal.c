45dc0fcfe4a4f2ab12da85e6aca3b6d105ee3803
souhailas142 <souhailas142@gmail.com>
2023-03-23 04:06:40 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i = 0, j;
+while (i < n)
+{
+j = 0;
+while (j < n)
+{
+if (i == j)
+_putchar('\\');
+else
+_putchar(' ');
+j++;
+}
+i++;
+}
+_putchar('\n');
+}
+}
