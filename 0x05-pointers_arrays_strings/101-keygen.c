21a4396890888be737b45a74cd4a99c84bf48012
souhailas142 <souhailas142@gmail.com>
2023-03-27 16:57:47 -0700

    random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,16 +7,18 @@
  */
 int main(void)
 {
-int i, nbr;
-i = nbr = 0;
-srand(time(0));
-while (i < 2772)
-{
-nbr = rand() % 128;
-if ((i + nbr) > 2772)
-break;
-i += nbr;
-printf("%c", nbr);
-}
-return (0);
+	int r = 0, c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
 }
