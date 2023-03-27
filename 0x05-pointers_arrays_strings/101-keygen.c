95f03590098d5543d2dba99267dffea223b3e95e
souhailas142 <souhailas142@gmail.com>
2023-03-27 16:40:35 -0700

    random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates random valid passwords
+ * Return: 0
+ */
+int main(void)
+{
+int i, nbr;
+i = nbr = 0;
+while (i < 2772)
+{
+nbr = rand() % 128;
+if ((i + nbr) > 2772)
+break;
+i += nbr;
+printf("%c", nbr);
+}
+return (0);
+}
