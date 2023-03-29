f4c39498c120be64f9657921da5901cfb91c20fa
souhailas142 <souhailas142@gmail.com>
2023-03-29 15:47:39 -0700

    prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,18 +6,20 @@
  */
 void print_number(int n)
 {
+unsigned int a;
 if (n < 0)
 {
 _putchar('-');
 n = n * -1;
 }
-if (n > 9)
+a = n;
+if (a > 9)
 {
-print_number(n / 10);
-print_number(n % 10);
+print_number(a / 10);
+print_number(a % 10);
 }
 else
 {
-_putchar(48 + n);
+_putchar(48 + a);
 }
 }
