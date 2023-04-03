5031f9258c0a0a41c86906dff8789efc7f11b66f
souhailas142 <souhailas142@gmail.com>
2023-04-03 08:57:10 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
  * _strpbrk - function that searches a string for any of a set of bytes
  * @s: string
  * @accept: string
- * Return: the first occurrence
+ * Return: string
  */
 char *_strpbrk(char *s, char *accept)
 {
