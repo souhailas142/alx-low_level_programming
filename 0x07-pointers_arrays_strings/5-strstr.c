9f284c8c5f924a6167f64915a1dfd45a404eda90
souhailas142 <souhailas142@gmail.com>
2023-04-03 06:38:32 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdbool.h>
+/**
+ * *_strstr - function that locates a substring
+ * @haystack: string
+ * @needle: string
+ * Return: string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0, j;
+bool found;
+while (*(needle + i) != '\0')
+{
+j = 0;
+while (*(haystack + j) != '\0')
+{
+if (*(needle + i) == *(haystack + j))
+{
+found = true;
+break;
+}
+j++;
+}
+if (found)
+{
+return (needle + i);
+}
+i++;
+}
+return (NULL);
+}
