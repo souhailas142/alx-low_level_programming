8f34cb61795a5a2466d5ff57386661982d45dc04
souhailas142 <souhailas142@gmail.com>
2023-04-03 14:41:56 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,10 +8,10 @@
 char *_strstr(char *haystack, char *needle)
 {
 int i = 0, j, k = 0;
-while (*(needle + i) >= '\0')
+while (*(haystack + i) >= '\0')
 {
 j = 0;
-while (*(haystack + j) >= '\0')
+while (*(needle + j) >= '\0')
 {
 if (*(needle + i) == *(haystack + j))
 {
@@ -22,7 +22,7 @@ j++;
 }
 if (k == 1)
 {
-return (haystack + j);
+return (needle + j);
 }
 i++;
 }
