4a72bdf24368e01321bd4bc72dd93bafd08fd6a3
souhailas142 <souhailas142@gmail.com>
2023-04-03 08:16:50 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,11 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0 ; s[i] != '\0' ; i++)
+int i = 0;
+while (*(s + i) != '\0')
 {
-if (s[i] == c)
+if (*(s + i) == c)
 return (s + i);
+i++;
 }
 return (NULL);
 }
