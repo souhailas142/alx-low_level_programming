96e9469c092a3d6b6e89448e97f8d3073d0921d1
souhailas142 <souhailas142@gmail.com>
2023-04-03 08:33:37 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,15 +7,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-		while (*s)
-		{
-			for (i = 0; accept[i]; i++)
-			{
-				if (*s == accept[i])
-					return (s);
-			}
-			s++;
-		}
-	return (NULL);
+int i = 0, j;
+while (*(s + i) != '\0')
+{
+j = 0;
+while (*(accept + j) != '\0')
+{
+if (*(s + i) == *(accept + j))
+{
+return (s + i);
+}
+j++;
+}
+i++;
+}
+return (NULL);
 }
