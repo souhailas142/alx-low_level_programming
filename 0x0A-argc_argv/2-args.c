980d3be4f54b2d7a7b43bce021fb57466a119929
souhailas142 <souhailas142@gmail.com>
2023-04-07 11:32:52 -0700

    .

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,34 @@
+#include "man.h"
+/**
+ * _puts - print string string string
+ * @s: string
+ * Return: nothing
+ */
+void _puts(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts(s + 1);
+}
+else
+{
+_putchar('\n');
+}
+}
+/**
+ * main - program that prints its name
+ * @argc: argument counter
+ * @argv: argument values
+ * Return: 0 (SUCCESS)
+ */
+int main(int argc, char *argv[])
+{
+int i = 0;
+while (i < argc)
+{
+_puts(argv[i]);
+i++;
+}
+return (0);
+}
