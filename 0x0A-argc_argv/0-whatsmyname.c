cccda16ab06174cf111376bdc19c577c448286cd
souhailas142 <souhailas142@gmail.com>
2023-04-07 09:05:39 -0700

    .

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,39 +1,13 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 /**
  * main - program that prints its name
  * @argc: argument counter
  * @argv: argument values
  * Return: 0 (SUCCESS)
  */
-/**
- * _putchar - writes the character
- * @c: character
- * Return: success
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-/**
- * _puts - print string
- * @s: string
- * Return: 0 (SUCCESS)
- */
-void _puts(char *s)
-{
-if (*s)
-{
-_putchar(*s);
-_puts(s + 1);
-}
-else
-{
-_putchar('\n');
-}
-}
 int main(int argc, char *argv[])
 {
-_puts(argv[argc - argc]);
+printf("%s\n", (argv[argc - argc]));
 return (0);
 }
