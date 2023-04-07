68e57b92fcfea33b08d50682ccd3ce1aeb2f3b65
souhailas142 <souhailas142@gmail.com>
2023-04-07 15:07:55 -0700

    .

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * print_number - print number
  * @n: number
@@ -21,54 +24,6 @@ print_number(k);
 _putchar((unsigned int) n % 10 + '0');
 }
 /**
- * _puts - print string
- * @s: string
- * Return: nothing
- */
-void _puts(char *s)
-{
-if (*s)
-{
-_putchar(*s);
-_puts(s + 1);
-}
-else
-{
-_putchar('\n');
-}
-}
-/**
- * _atoi - convert nbr string to number
- * @s: string
- * Return: number or 0
- */
-int _atoi(char *s)
-{
-int number = 0, i = 0;
-int sg = 1;
-int size = 0;
-while (s[size] != '\0')
-size++;
-while (i < size)
-{
-if (s[i] == '-')
-{
-sg = -1;
-}
-else
-if (s[i] >= '0' && s[i] <= '9')
-{
-number = 10 * number + (s[i] - 48);
-}
-else
-{
-return (0);
-}
-i++;
-}
-return (number *sg);
-}
-/**
  * main - program that adds positive numbers
  * @argc: argument counter
  * @argv: argument values
@@ -82,13 +37,13 @@ if (argc > 1)
 int i = 1;
 while (i < argc)
 {
-if (_atoi(argv[i]) != 0)
+if (atoi(argv[i]) != 0)
 {
-add += _atoi(argv[i]);
+add += atoi(argv[i]);
 }
 else
 {
-_puts("Error");
+puts("Error");
 return (1);
 }
 i++;
