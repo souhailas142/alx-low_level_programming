147dfe1ccb6ceafcc1f2729ff0eeace8b8e5d25e
souhailas142 <souhailas142@gmail.com>
2023-04-07 15:32:40 -0700

    .

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _atoi - convert nbr string to number
+ * @s: string
+ * Return: number or 0
+ */
+int _atoi(char *s)
+{
+int number = 0, i = 0;
+int sg = 1;
+int size = 0;
+while (s[size] != '\0')
+size++;
+while (i < size)
+{
+if (s[i] == '-')
+{
+sg = -1;
+}
+else
+if (s[i] >= '0' && s[i] <= '9')
+{
+number = 10 * number + (s[i] - 48);
+}
+else
+{
+return (0);
+}
+i++;
+}
+return (number *sg);
+}
+/**
  * main - program that adds positive numbers
  * @argc: argument counter
  * @argv: argument values
@@ -16,9 +47,9 @@ if (argc > 1)
 i = 1;
 while (i < argc)
 {
-if (atoi(argv[i]) != 0)
+if (_atoi(argv[i]) != 0)
 {
-add += atoi(argv[i]);
+add += _atoi(argv[i]);
 }
 else
 {
