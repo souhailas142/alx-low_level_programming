a75a2854147e1cab303e71a277b5f65dfc546900
souhailas142 <souhailas142@gmail.com>
2023-04-11 11:07:01 -0700

    .

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,10 +11,12 @@ char *_strdup(char *str)
 	int len = 0, i = 0;
 	char *s;
 
+	if (str == NULL)
+		return (0);
 	while (str[len] != '\0')
 		len++;
 	s = (char *)malloc(len * sizeof(*str) + 1);
-	if (s == 0 || str == 0)
+	if (s == 0)
 		return (0);
 	while (i < len)
 	{
