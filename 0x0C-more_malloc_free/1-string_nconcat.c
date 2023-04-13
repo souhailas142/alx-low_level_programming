88e414f2ed8f356c62818a5b27d31bde96eb0b67
souhailas142 <souhailas142@gmail.com>
2023-04-13 06:15:17 -0700

    .

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <sdlib.h>
+/**
+ * _strlen -  function calculates the length of a given string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes
+ * Return: SUCCESS
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j, len1, len2, lent;
+	char *str;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (len1 == 0 || len2 == 0)
+		return (0);
+	if (n >= len2)
+		n = len2;
+	lent = len1 + n;
+	str = malloc((lent + 1) * sizeof(char *));
+	if (str == NULL)
+		return (0);
+	i = 0;
+	j = 0;
+	while (i < lent)
+	{
+		if (i < len1)
+			str[i] = s1[i];
+		else
+		{
+			str[i] = s2[j];
+			j++;
+		}
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
