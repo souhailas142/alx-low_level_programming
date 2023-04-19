9c6622e199a9cf1df78b2047383a68618e0fd730
souhailas142 <souhailas142@gmail.com>
2023-04-19 03:07:56 -0700

    .

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,20 +1,5 @@
 #include "3-calc.h"
 /**
- * _strcmp - cmp two string
- * @s1: str1
- * @s2: str2
- * Return: SUCCESS
- */
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
-}
-/**
  * get_op_func -  corresponds the operator given as a parameter
  * @s: op
  * Return: pointer to the function or NULL
@@ -34,7 +19,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op)
 	{
-		if (_strcmp(ops[i].op, s) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
