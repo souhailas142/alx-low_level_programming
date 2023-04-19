40e83533a3a6586e1159ad96a9024a814f80ae80
souhailas142 <souhailas142@gmail.com>
2023-04-19 02:48:03 -0700

    .

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,10 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
+
 	if (!array || !size || !action)
 		return;
-	size_t i = 0;
-
+	i = 0;
 	while (i < size)
 	{
 		action(array[i]);
