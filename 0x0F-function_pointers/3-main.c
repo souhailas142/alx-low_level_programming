d6a9023631f1318e8e1cb0a76328d752f62bb081
souhailas142 <souhailas142@gmail.com>
2023-04-19 15:27:30 -0700

    .

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,7 +8,6 @@
 int main(int argc, char *argv[])
 {
 	int nbr1, nbr2, rslt;
-	char op;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -16,26 +15,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
 	nbr1 = atoi(argv[1]);
 	nbr2 = atoi(argv[3]);
-
 	func = get_op_func(argv[2]);
-
 	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	op = *argv[2];
-
-	if ((op == '/' || op == '%') && nbr2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	rslt = func(nbr1, nbr2);
 	printf("%d\n", rslt);
 	return (0);
