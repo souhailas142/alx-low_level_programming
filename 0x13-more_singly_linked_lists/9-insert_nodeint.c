9316cb7856a8dc4677f354ba52d5cde59113f5c6
souhailas142 <souhailas142@gmail.com>
2023-04-29 08:13:33 -0700

    .

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while (idx - 1 && t)
+	while ((idx - 1) && (t != NULL))
 	{
 		t = t->next;
 		idx--;
