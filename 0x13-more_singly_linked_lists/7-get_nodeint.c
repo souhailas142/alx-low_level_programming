60c88bcff164e37eb2fa5d5fe88bf1d699ee602f
souhailas142 <souhailas142@gmail.com>
2023-04-29 07:17:38 -0700

    .

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,6 +12,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (NULL);
 	node = head;
+	if (index == 0)
+		return (head);
 	while (index)
 	{
 		node = node->next;
