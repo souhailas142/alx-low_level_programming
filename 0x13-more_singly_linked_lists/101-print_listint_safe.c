da7ccc0e8d1948e5cbacbaf9894328cb081facc0
souhailas142 <souhailas142@gmail.com>
2023-05-01 08:34:52 -0700

    .

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,23 +1,59 @@
 #include "lists.h"
 /**
+ * _f - funct to reallocates memory
+ * @list: old list
+ * @size: size
+ * @new: new node
+ * Return: pointer to the new list
+ */
+const listint_t **_f(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **n;
+	size_t i;
+
+	n = malloc(size * sizeof(listint_t *));
+	if (!n)
+	{
+		free(list);
+		exit(98);
+	}
+	i = 0;
+	while (i < (size - 1))
+	{
+		n[i] = list[i];
+		i++;
+	}
+	n[i] = (listint_t *)new;
+	free(list);
+	return (n);
+}
+/**
  * print_listint_safe - print listint safe
  * @head: head
  * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *h;
-	int nbrNode;
+	size_t i, nbrNode;
+	const listint_t **list = NULL;
 
-	if (!head)
-		exit(98);
-	h = (listint_t *)head;
-	nbrNode = 0;
-	while (h)
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)h, h->n);
+		i = 0;
+		while (i < nbrNode)
+		{
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (nbrNode);
+			}
+			i++;
+		}
 		nbrNode++;
-		h = h->next;
+		list = _f(list, nbrNode, head);
+		printf("[%p] %d\n", (void *)head, head->n);
 	}
+	free(list);
 	return (nbrNode);
 }
