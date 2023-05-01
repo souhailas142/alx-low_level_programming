8ef3d96ec51f4e2a9696c4bc7ca7a62016865147
souhailas142 <souhailas142@gmail.com>
2023-05-01 09:45:44 -0700

    .

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,28 +1,28 @@
 #include "lists.h"
 /**
- * loop_listint_t - loop in linked list
+ * find_listint_loop - loop in linked list
  * @head: head
  * Return: SUCCESS or NULL if no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-        listint_t *ptr, *end;
+	listint_t *ptr, *end;
 
-        if (head == NULL)
-                return (NULL);
-        end = head->next;
-        while (end)
-        {
-                if (end == end->next)
-                        return (end);
-                ptr = head;
-                while (ptr != end)
-                {
-                        if (ptr == end->next)
-                                return (end->next);
-                        ptr = ptr->next;
-                }
-                end = end->next;
-        }
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
+	end = head->next;
+	while (end)
+	{
+		if (end == end->next)
+			return (end);
+		ptr = head;
+		while (ptr != end)
+		{
+			if (ptr == end->next)
+				return (end->next);
+			ptr = ptr->next;
+		}
+		end = end->next;
+	}
+	return (NULL);
 }
