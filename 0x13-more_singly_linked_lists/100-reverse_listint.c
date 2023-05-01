52aa912d8b710d29a9fac7082afa8286926c58a4
souhailas142 <souhailas142@gmail.com>
2023-04-30 23:11:35 -0700

    .

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a @listint_t
+ * @head: head
+ * Return: reverses a @listint_t
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *t1;
+	listint_t *t2;
+	if (!head || !(*head))
+		return (NULL);
+	t1 = *head;
+	*head = NULL;
+	while (t1)
+	{
+		t2 = t1->next;
+		t1->next = *head;
+		*head = t1;
+		t1 = t2;
+	}
+	return (*head);
+}
