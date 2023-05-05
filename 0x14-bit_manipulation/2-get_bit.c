e787448654da9b00b497b724a235a4e7cea07f3e
souhailas142 <souhailas142@gmail.com>
2023-05-05 11:34:10 -0700

    .

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int getB;
+
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	return (n >> index & 1);
+	get = n >> index;
+	return (get & 1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	if (*n  & 1L << index)
-		*n ^= 1L << index;
+	*n &= ~(1UL << index);
 	return (1);
 }
