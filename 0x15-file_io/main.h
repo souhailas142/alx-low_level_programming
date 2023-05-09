4615c9a81594dfedc1a8872494aa8122fa07b44d
souhailas142 <souhailas142@gmail.com>
2023-05-09 08:59:50 -0700

    .

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,7 +11,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void f_error(int ff, int ft, char *argv[]);
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
