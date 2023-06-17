011c59e4453e1b0ed673f023ac305dd0f9b06d11
souhailas142 <souhailas142@gmail.com>
2023-06-17 02:09:12 -0700

    .

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,9 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		t1 = t1->next;
 		i++;
 	}
-	if (t1 == NULL)
-	       return (NULL);
-	if (t1->next == NULL)
+	if (t1 == NULL || t1->next == NULL)
 		return (add_dnodeint_end(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
